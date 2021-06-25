53d7c65126293cb34ba6bf008e2568d3e39197a8
Holiday Man <lesha.pitenko791@gmail.com>
2021-06-25 15:01:03 +0300

    added django-cors-headers package

--- notes/settings.py
+++ notes/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'djoser',
     'app.notes_main',
 ]
@@ -122,6 +123,7 @@ USE_L10N = True
 
 USE_TZ = True
 
+CORS_ALLOW_ALL_ORIGINS = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
